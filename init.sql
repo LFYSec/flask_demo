8e3d557a4f9e5a6e3ce19ae18e1c23e653eced6f
ShadowGlint <33030694+ShadowGlint@users.noreply.github.com>
2018-05-01 23:43:37 +0800

    Delete init.sql

--- init.sql
+++
@@ -1,8 +0,0 @@
-drop table if exists users;
-create table users(
-	id integer primary key autoincrement,
-	username text not null,
-	passwd text not null
-);
-
-insert into users (username,passwd) values ('myoung','lfy123'),('ShadowGlint','lfy321');
